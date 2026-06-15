36df6060da11470aa050643b3bb22737de9ad08a
Interraqt <hardikkalal360@gmail.com>
2026-06-15 12:16:52 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
+
+// Import Screens
 import LoginScreen from './src/screens/LoginScreen';
+import HomeScreen from './src/screens/HomeScreen';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <>
-      <LoginScreen />
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        {/* The first screen the app loads */}
+        <Stack.Screen name="Login" component={LoginScreen} />
+        {/* The screen they go to after logging in */}
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
       <StatusBar style="auto" />
-    </>
+    </NavigationContainer>
   );
 }
