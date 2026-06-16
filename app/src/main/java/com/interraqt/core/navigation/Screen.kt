b0cba59701b2cad543ef3d20035eed676d3a9965
Interraqt <hardikkalal360@gmail.com>
2026-06-16 23:23:10 +0530

    Update Screen.kt

--- app/src/main/java/com/interraqt/core/navigation/Screen.kt
+++ app/src/main/java/com/interraqt/core/navigation/Screen.kt
@@ -2,12 +2,13 @@ package com.interraqt.core.navigation
 
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
+import androidx.compose.material.icons.outlined.*
 import androidx.compose.ui.graphics.vector.ImageVector
 
-sealed class Screen(val route: String, val title: String, val icon: ImageVector) {
-    object Home : Screen("home", "Home", Icons.Default.Home)
-    object Chat : Screen("chat", "Chat", Icons.Default.Chat)
-    object Explore : Screen("explore", "Explore", Icons.Default.Search)
-    object Video : Screen("video", "Video", Icons.Default.PlayArrow)
-    object Profile : Screen("profile", "Profile", Icons.Default.Person)
+sealed class Screen(val title: String, val selectedIcon: ImageVector, val unselectedIcon: ImageVector) {
+    object Home : Screen("Home", Icons.Filled.Home, Icons.Outlined.Home)
+    object Chat : Screen("Chat", Icons.Filled.ChatBubble, Icons.Outlined.ChatBubbleOutline)
+    object Explore : Screen("Explore", Icons.Filled.Search, Icons.Outlined.Search)
+    object Video : Screen("Video", Icons.Filled.PlayCircle, Icons.Outlined.PlayCircleOutline)
+    object Profile : Screen("Profile", Icons.Filled.Person, Icons.Outlined.Person)
 }
