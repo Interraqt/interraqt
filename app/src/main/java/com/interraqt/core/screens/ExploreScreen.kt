a58528f81a178992cc89a3f9b8fddbdba1bdf895
Interraqt <hardikkalal360@gmail.com>
2026-06-16 23:04:42 +0530

    Create ExploreScreen.kt

---
+++ app/src/main/java/com/interraqt/core/screens/ExploreScreen.kt
@@ -0,0 +1,6 @@
+package com.interraqt.core.screens
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.*
+@Composable fun ExploreScreen() { Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) { Text("Explore Feed") } }
